187d341df2944ea894af0308659c0f6a14226b48
Nastya Podtyagina <nastya.mytareva.04@gmail.com>
2024-12-15 00:19:58 +0400

    rename foulder lab->lab1, logging has been performed in working_functions.py

--- lab_4/lab1/const.py
+++ lab_4/lab1/const.py
@@ -1,3 +1,4 @@
 PATH = "lab_1/paths.json"
 ALPHABET = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+LOG = "lab1/app.log"
  
--- lab_4/lab1/working_functions.py
+++ lab_4/lab1/working_functions.py
@@ -1,48 +1,62 @@
 import json
+import logging
 
+from const import LOG
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", filename=LOG, filemode="a") 
 
 def read_file(path: str) -> str:
     """
     A function for reading a file. accepts a path as input, returns a string.
     """
+    logging.info(f"Reading file: {path}")
     try:
         with open(path, "r", encoding="utf-8") as f:
             text = f.read()
+        logging.info(f"File '{path}' read successfully.")
         return text
     except FileNotFoundError as e:
-        print(e)
+        logging.error(f"Error reading file '{path}': {e}")
+        return None
 
 
 def write_file(path: str, text: str) -> None:
     """
     A function for writing to a file using a specified path.
     """
+    logging.info(f"Writing to file: {path}")
     try:
         with open(path, "w", encoding="utf-8") as f:
             f.write(text)
+        logging.info(f"File '{path}' written successfully.")
     except Exception as e:
-        print(e)
+        logging.exception(f"An error occurred while writing to '{path}': {e}")
 
 
 def read_json(path: str) -> dict:
     """
     A function for reading a .json file. accepts a path as input, returns a dictionary.
     """
+    logging.info(f"Reading .json file: {path}")
     try:
         with open(path, "r", encoding="UTF-8") as f:
             text = json.load(f)
+        logging.info(f"A .json file '{path}' read successfully.")
         return text
     except FileNotFoundError as e:
-        print(e)
+        logging.error(f"Error reading .json file '{path}': {e}")
+        return None
 
 
 def write_json(path: str, dictionary: dict) -> None:
     """
     A function for writing to a file using a specified path.
     """
+    logging.info(f"Writing to .json file: {path}")
     try:
         with open(path, 'w') as f:
             json.dump(dictionary, f)
+        logging.info(f"A .json file '{path}' written successfully.")
     except Exception as e:
-        print(e)
+        logging.exception(f"An error occurred while writing to '{path}': {e}")
  
