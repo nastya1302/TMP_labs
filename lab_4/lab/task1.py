c98686eb1d7436b2c8d312f5dfc0252ab039bcf5
Nastya Podtyagina <nastya.mytareva.04@gmail.com>
2024-12-14 22:14:51 +0400

    source code

---
+++ lab_4/lab/task1.py
@@ -0,0 +1,53 @@
+from working_functions import *
+from const import *
+
+
+def create_dict(sourse_alphabet: str) -> dict:
+    """
+    A function for creating a dictionary from the alphabet. Accepts the alphabet as input, returns the dictionary.
+    """
+    dict_alphabet: dict = {}
+    i: int = 0
+    for letter in sourse_alphabet:
+        dict_alphabet[letter] = i
+        i += 1
+    return dict_alphabet
+
+
+def create_vigenere_ciphe(sourse_alphabet: str) -> dict:
+    """
+    A function for creating vigenere ciphe. Accepts the alphabet as input, returns the dictionary.
+    """
+    list_letter: list = list(sourse_alphabet)
+    list_line: dict = {}
+    for i in range(32):
+        list_line[i] = list_letter[i : i + 32 : 1] + list_letter[0:i]
+    return list_line
+
+
+def encryption(path_sourse_text: str, path_key: str, path_encrypted_text: str) -> None:
+    """
+    A function for creating encrypted text.
+    """
+    text: str = read_file(path_sourse_text)
+    key: str = read_file(path_key)
+    text: str = "".join(text.split())
+    key: str = (key * (len(text) // len(key) + 1))[: len(text)]
+    dict_alphabet: dict = create_dict(ALPHABET)
+    table: dict = create_vigenere_ciphe(ALPHABET)
+    new_text: str = ""
+    for k, t in zip(key, text):
+        new_text += table[dict_alphabet[k]][dict_alphabet[t]]
+    write_file(path_encrypted_text, new_text)
+
+
+def main() -> None:
+    paths: dict = read_json(PATH)
+    encryption(
+        paths["task1_sourse_text"], paths["task1_key"], paths["task1_encrypted_text"]
+    )
+
+
+if __name__ == "__main__":
+    main()
+ 
---
+++ lab_4/lab/task2.py
@@ -0,0 +1,43 @@
+from working_functions import *
+from const import PATH
+
+
+def frequency_analysis_of_text(path: str, text: str) -> None:
+    """
+    A function for performing frequency analysis of a given ciphertext.
+    """
+    frequency = {}
+    l = len(text)
+    text_litters = []
+    for i in text:
+        if text_litters.count(i) == 0:
+            text_litters.append(i)
+    for i in text_litters:
+        frequency[i] = text.count(i) / l
+    write_json(path, frequency)
+
+
+def decryption(path_sourse_text: str, path_key: str, path_encrypted_text: str, path_text_analysis: str) -> None:
+    """
+    A function for creating decrypted text.
+    """
+    text: str = read_file(path_encrypted_text)
+    key: dict = read_json(path_key)
+    frequency_analysis_of_text(path_text_analysis, text)
+    new_text: str = ""
+    for letter in text:
+        if letter in key:
+            new_text += key[letter]
+    write_file(path_sourse_text, new_text)
+
+
+def main() -> None:
+    paths: dict = read_json(PATH)
+    decryption(
+        paths["task2_sourse_text"], paths["task2_key"], paths["task2_encrypted_text"], paths["task2_text_analysis"]
+    )
+
+
+if __name__ == "__main__":
+    main()
+ 
